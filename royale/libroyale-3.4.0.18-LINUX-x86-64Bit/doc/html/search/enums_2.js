dd69aa8c247ebc67e3ccd93c9930f67f9efd81f2
Johannes <johannes.loje@flyability.com>
2020-02-04 14:11:21 -0800

    Added royale library for arm-64

--- royale/libroyale-3.4.0.18-LINUX-x86-64Bit/doc/html/search/enums_2.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['imagesensorlogtype',['ImageSensorLogType',['../namespace_royale_dot_net.html#a1fd5f2748bff3f877907caf9fc498c87',1,'RoyaleDotNet']]]
-];
--- royale/libroyale-3.4.0.18-LINUX-x86-64Bit/doc/html/search/functions_13.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['writecalibrationtoflash',['writeCalibrationToFlash',['../classroyale_1_1_i_camera_device.html#a3f5437ed7844b2d1eb62f11e2f6e6d30',1,'royale::ICameraDevice::writeCalibrationToFlash()'],['../class_royale_dot_net_1_1_camera_device.html#ae47ebabf19c82ab640ff99d85db13022',1,'RoyaleDotNet.CameraDevice.WriteCalibrationToFlash()']]],
-  ['writeregisters',['writeRegisters',['../classroyale_1_1_i_camera_device.html#a7c1c376d0e7bf90d0815ba037c6805b7',1,'royale::ICameraDevice::writeRegisters()'],['../class_royale_dot_net_1_1_camera_device.html#ae860b675d8c1d0f4f2f9925a1a524c65',1,'RoyaleDotNet.CameraDevice.WriteRegisters()']]]
-];
