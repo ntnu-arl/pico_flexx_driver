c0b63fdf4278a8c96aec3542f75255f1940d5660
tiralonghipol <tiralonghipol@gmail.com>
2020-06-08 10:12:16 -0700

    raspi4 branch

--- royale/libroyale-3.4.0.18-LINUX-x86-64Bit/doc/html/search/variables_a.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['phaseangles',['phaseAngles',['../structroyale_1_1_raw_data.html#aa04748cee652443bd7bd90f18b7711c5',1,'royale::RawData']]],
-  ['points',['points',['../structroyale__depth__data.html#a834b3b139a17351ce4284fe6c581babb',1,'royale_depth_data::points()'],['../structroyale__intermediate__data.html#abcbcc53dfe6effe1c90cd2547b4c9164',1,'royale_intermediate_data::points()'],['../struct_royale_dot_net_1_1_depth_data.html#a3380e902953023f1e788d57601226940',1,'RoyaleDotNet.DepthData.points()'],['../struct_royale_dot_net_1_1_intermediate_data.html#a997ca2b085d7f5710a86c8dbd53d3a3c',1,'RoyaleDotNet.IntermediateData.points()'],['../structroyale_1_1_depth_data.html#ad694feb7746bb2b4a7aa0b804045176d',1,'royale::DepthData::points()'],['../structroyale_1_1_intermediate_data.html#add8c19946a6c88af336ab4a5fdc4fbd7',1,'royale::IntermediateData::points()']]],
-  ['principalpoint',['principalPoint',['../structroyale__lens__parameters.html#a3bcddd0e94650054dd4cc86401266745',1,'royale_lens_parameters::principalPoint()'],['../structroyale_1_1_lens_parameters.html#a1af95893c2d205c4410c54bf2ed3267b',1,'royale::LensParameters::principalPoint()']]]
-];
